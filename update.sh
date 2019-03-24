7d3bf3118d3b01f2ea8c11001e3bfd3acbb99836
Isamyh <isakmyhrvold@gmail.com>
2019-03-24 11:33:54 +0100

    Documentation update

--- update.sh
+++ update.sh
@@ -3,10 +3,10 @@ echo "Pulling latest git"
 git pull git@github.com:isamyh/Ungdomsklubb-server.git
 echo "Updating dokumentasjon.adoc"
 asciidoctor -a toc dokumentasjon.adoc
-asciidoctor -a pdf-stylesdir=resources/themes -a pdf-style=basic -a pdf-fontsdir=resources/fonts -r asciidoctor-pdf -b pdf -a toc dokumentasjon.adoc
+asciidoctor -r asciidoctor-pdf -b pdf -a toc dokumentasjon.adoc
 echo "Updating logg.adoc"
 asciidoctor -a toc logg.adoc
-asciidoctor -a pdf-stylesdir=resources/themes -a pdf-style=basic -a pdf-fontsdir=resources/fonts -r asciidoctor-pdf -b pdf -a toc logg.adoc
+asciidoctor -r asciidoctor-pdf -b pdf -a toc logg.adoc
 echo "Commiting changes"
 git add *
 git commit -m "Documentation update"
